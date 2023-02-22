7fcc602c9181888d9a7598481937f7271faf4b67
Madzeedy <harunamadjid226@gmail.com>
2023-02-22 23:27:45 +0200

    second commit

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
       <div className="gpt3_blog-container-groupA">
         <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
       </div>
-      <div className="gpt3_blog-container_groupB">
+      <div className="gpt3_blog-container-groupB">
         <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
         <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
         <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
